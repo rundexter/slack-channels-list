c3984951fdf77a9cebf9536d78ad67ea7f22aa84
Daniel Ilkovich <daniel+github@betaworks.com>
2015-07-07 16:28:36 -0400

    initial

--- fixtures/default.js
+++ fixtures/default.js
@@ -4,35 +4,17 @@ var _ = require('lodash')
 
 module.exports = _.merge({
     simulation: true
-    , instance_id: 'local_test_instance'
+    , instance_id : 'local_test_instance'
     , urls: {
-        home: "http://rundexter.com/"
+        home : "http://rundexter.com/"
     }
     , instance_state: {
         active_step :  "local_test_step"
     }
     , workflow: {
-        "id" : "local_test_workflow"
-        , "title": "Local test workflow"
-        , "description": "A fixture workflow used to test a module"
-    }
-    , environment: {
-       /*
-        * Any API keys you might need should go in the env.js file found in the same
-        * folder as this fixture.  For example:
-        *
-        "parse_app_id": "abc123"
-        , "parse_app_key": "foobar"
-        */
-    }
-    , user: {
-        /*
-         * Your dexter user settings should go in the env.js file.  For example:
-        profile: {
-            email: "joe@test.com"
-            , username: "joe"
-        }
-         */
+        "id"            : "local_test_workflow"
+        , "title"       : "Local test workflow"
+        , "description" : "A fixture workflow used to test a module"
     }
     , steps: {
         local_test_step: {
@@ -44,13 +26,11 @@ module.exports = _.merge({
         }
     }
     , modules: {
-        //The test runner will add the proper data here
     }
     , data: {
         local_test_step: {
             input: {
-                //Replace VAR1 with the name of an expected input, and add more inputs as needed.
-                VAR1: 'foo'
+                exclude_nonmember: true
             }
         }
     }
--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+var rest = require('restler')
+  , _    = require('lodash')
+  , q    = require('q')
+;
+
 module.exports = {
     /**
      * This optional function is called every time before the module executes.  It can be safely removed if not needed.
@@ -12,8 +17,52 @@ module.exports = {
      * @param {WFDataParser} dexter Container for all data used in this workflow.
      */
     , run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
-    }
+        var exclude_nonmember = step.input('exclude_nonmember').first()
+          , exclude_archived  = step.input('exclude_archived').first()
+          , provider    = dexter.provider('slack')
+          , botToken    = provider.data('bot.bot_access_token')
+          , isBot       = !!botToken //operate as the bot if we don't have a different username and we have a bot token
+          , accessToken =  botToken || provider.credentials('access_token')
+          , url         = 'https://slack.com/api/channels.list'
+          , self        = this
+          , data        = {
+              exclude_archived: exclude_archived || 1
+              , token    : accessToken
+          }
+        ;
+
+        console.log(data);
+
+        this.send(data, url)
+           .then(function(results) {
+               if(exclude_nonmember) {
+                   self.complete(_.where(results.channels, { is_member: true }));
+               } else {
+                   self.complete(results.channels);
+               }
+           })
+           .catch(function(err) {
+               self.fail(err);
+           });
+   }
+   , send: function(data, url) {
+        var deferred = q.defer();
+        rest.post(url, {data:data}).on('complete', function(result, response) {
+            if(result instanceof Error) {
+                return deferred.reject(result.stack || result);
+            }
+            if(response.statusCode !== 200) {
+                return deferred.reject({
+                    message: 'Error Result From Slack',
+                    code: response.statusCode,
+                    postData: data 
+                });
+            }
+            return deferred.resolve(_.merge(
+                _.isObject(result) ? result : { result: result }
+                , data
+            ));
+        });
+        return deferred.promise;
+   }
 };
